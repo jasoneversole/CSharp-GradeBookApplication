12bd6e50e915f29ca4a9ccd0083847e9ffa17456
Jason Eversole <jason.eversole@gmail.com>
2018-03-21 13:29:03 -0400

    Step 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
@@ -10,6 +11,42 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked Grading requires a minimum of 5 students to work");
+            }
+
+            int rankCount = Convert.ToInt32(Students.Count * 0.20f);
+            Students = Students.OrderByDescending(x => x.AverageGrade).ToList();            
+
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (Students[i].AverageGrade == averageGrade)
+                {
+                    if((i + 1) <= rankCount)
+                    {
+                        return 'A';
+                    }
+                    else if((i + 1) <= rankCount*2)
+                    {
+                        return 'B';
+                    }
+                    else if ((i + 1) <= rankCount * 3)
+                    {
+                        return 'C';
+                    }
+                    else if ((i + 1) <= rankCount * 4)
+                    {
+                        return 'D';
+                    }
+                }
+            }
+
+            return 'F';
         }
     }
 }
